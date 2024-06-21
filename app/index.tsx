e2ffba179aaf4d386e86d18f7f37cac1f22c626f
dfergusbrown <dfergusbrown@gmail.com>
2024-06-21 02:17:12 -0400

    adds game-boy-like view screen and custom font

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,29 @@
+import { useFonts } from "expo-font";
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+
+  const [loaded] = useFonts({
+    DeluxePaintComic: require('../assets/fonts/dpcomic/dpcomic.ttf'),
+    RexliaFree: require('../assets/fonts/rexlia-free/rexliarg.otf')
+  })
+
+  if (!loaded) {
+    return (
+      <Text>Loading</Text>
+    );
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "red"
-      }}
-    >
+    <View style={styles.container}>
       <ImageBackground 
       source={require('../assets/images/pokedex_background.jpg')}
       style={styles.backgroundImage}
       resizeMode="stretch"
       >
-        {/* <Text>Edit app/index.tsx to edit this screen.</Text> */}
+        <View style={styles.grayScreen}>
+          <Text style={styles.textTitle}>Pokedexter</Text>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -27,8 +35,22 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
-    justifyContent: 'center',
-    width: '100%', // Ensures the image stretches to the width of the container
-    height: '100%', // Ensures the image stretches to the height of the container
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
   },
+  grayScreen: {
+    height: '30%',
+    width: '60%',
+    marginTop: '45%',
+    backgroundColor: 'silver',
+    borderWidth: 1,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textTitle: {
+    fontFamily: 'RexliaFree',
+    fontSize: 25
+  }
 });
