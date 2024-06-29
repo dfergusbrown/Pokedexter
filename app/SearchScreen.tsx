ccadaf1c743aa77462f1ea4e9ff4aac11f26016b
dfergusbrown <dfergusbrown@gmail.com>
2024-06-29 11:41:37 -0400

    pokemon list filters when searched

--- app/SearchScreen.tsx
+++ app/SearchScreen.tsx
@@ -1,7 +1,8 @@
 import BackgroundPokedex from "@/components/BackgroundPokedex";
 import { getAllPokemon } from "@/services/apiService";
 import { useEffect, useState } from "react";
-import { Text, View, FlatList, ListRenderItem } from "react-native";
+import { Text, View, FlatList, ListRenderItem, StyleSheet, Pressable } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 interface Pokemon {
   name: string;
@@ -9,41 +10,96 @@ interface Pokemon {
 }
 
 const SearchScreen = () => {
-  const [pokedata, setPokedata] = useState([]);
+  const [pokedata, setPokedata] = useState<Pokemon[]>([]);
+  const [filteredResult, setFilteredResult] = useState<Pokemon[]>([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
     async function fetchPokemon() {
-      const response = await getAllPokemon();
-      if (response.status === 200) {
-        setPokedata(response.data.results);
-        console.log(response.data.results);
+      try {
+        const response = await getAllPokemon();
+        if (response.status === 200) {
+          setPokedata(response.data.results);
+          setFilteredResult(response.data.results)
+          console.log(response.data.results);
+        }        
+      } catch (error) {
+        console.error(error)
       }
     }
     fetchPokemon();
   }, []);
 
+  useEffect(() => {
+    const filterPokemon = () => {
+      if (!searchTerm) {
+        
+      }
+      const filteredData = pokedata.filter(item => {
+        return item.name.includes(searchTerm)
+      })
+      setFilteredResult(filteredData)
+    }
+    filterPokemon()
+  }, [searchTerm])
+
   const renderList: ListRenderItem<Pokemon> = ({ item }) => {
     return (
-      <View>
-        <Text>{item.name}</Text>
-      </View>
+      <Pressable>
+        <View style={styles.listItem}>
+          <Text style={styles.listText}>{item.name}</Text>
+        </View>
+      </Pressable>
     );
   };
 
   return (
     <BackgroundPokedex>
+      <View style={styles.inputContainer}>
+        <TextInput 
+          value={searchTerm}
+          onChangeText={setSearchTerm}
+          style={styles.input}
+        />
+      </View>
       <View
-        style={{
-          flex: 1,
-          width: '70%',
-          marginTop: 150,
-          backgroundColor: 'white'
-        }}
+        style={styles.listContainer}
       >
-        <FlatList data={pokedata} renderItem={renderList} />
+        <FlatList data={filteredResult} renderItem={renderList} />
       </View>
     </BackgroundPokedex>
   );
 };
+
+const styles = StyleSheet.create({
+  listContainer: {
+    flex: 1,
+    width: '75%',
+    marginTop: 10,
+    marginLeft: -20,
+    backgroundColor: 'white',
+    borderRadius: 5
+  },
+  listItem: {
+    padding: 4
+  },
+  listText: {
+    fontSize: 20
+  },
+  inputContainer: {
+    marginTop: 150,
+    marginLeft: 50,
+    width: '100%'
+  },
+  input: {
+    height: 40,
+    width: '75%',
+    marginLeft: 15,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'white',
+    borderRadius: 5
+  }
+})
 
 export default SearchScreen;
